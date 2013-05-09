ea23712d4e98071569739d5d60fb68f70faf3f23
Krzysztof Miąsko <kmiasko@kmiasko.eu>
2013-05-10 00:04:14 +0200

    some generate_barcode refactorings, added static folder, style.css

--- lib/barcode_generator.py
+++ lib/barcode_generator.py
@@ -8,13 +8,16 @@ def generate_barcode(barcode):
         svg_document = svgwrite.Drawing(filename = "test-svgwrite.svg", size = ("800px", "600px"))
         start_x = 50
         start_y = 50
+        bar_size = ("2px", "70px")
         x = 0
 
         for b in binary:
             if b == '1':
-                svg_document.add(svg_document.rect(insert = (start_x + x, start_y), size = ("2px", "70px"), stroke_width = "0", stroke = "black", fill = "rgb(0,0,0)"))
+                svg_document.add(svg_document.rect(insert = (start_x + x, start_y), size = bar_size, fill = "rgb(0,0,0)"))
             else:
-                svg_document.add(svg_document.rect(insert = (start_x + x, start_y), size = ("2px", "70px"), stroke_width = "0", stroke = "black", fill = "rgb(255,255,255)"))
+                svg_document.add(svg_document.rect(insert = (start_x + x, start_y), size = bar_size, fill = "rgb(255,255,255)"))
             x += 2
         svg_document.add(svg_document.text(barcode, insert = (start_x + 50, start_y + 90)))
         return svg_document.tostring()
+    else:
+        return None
